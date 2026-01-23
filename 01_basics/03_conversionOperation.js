24988707674ae8608564781e263023409519724d
Garv Gupta <garvg8423@gmail.com>
2026-01-23 05:56:13 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,3 +34,27 @@ console.log(valueInNumber3) // 0
 // " " -> 0, false
 //1-> true
 // "GARV" -> true
+
+
+
+/***************** Operations ********************/
+
+console.log ( 5 + 5 ) //10
+console.log (5 * 5) //25
+console.log ( 5 ** 5) // 5^5 = 3125
+console.log ( 5 - 5) //0
+console.log ( 5 / 5) //1
+console.log ( 5 % 5) //0
+
+// + operator is also used for string concatenation
+
+console.log ("1" + 2) // "12"
+console.log (1 + "2") // "12"
+console.log (1 + 2 + "3") // "33"
+console.log ("3" + 1 + 2) // "312"
+
+// if sting first then all treated as string
+// if number first then all treated as number until string comes
+
+console.log( +true ) // 1, bad practice, better use Number(true)
+console.log(true +) // error
