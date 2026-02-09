8bedac6c22a37323836ea32f8a0f408d474ed081
Garv Gupta <garvg8423@gmail.com>
2026-02-09 18:10:23 +0000

    Add student object with city update and country addition examples

---
+++ 06_objectLiteral/6.3_addUpdateValue.js
@@ -0,0 +1,16 @@
+const student = {
+    name: "Garv",
+    age: 23,
+    marks: 94.4
+    city: "Jhansi"
+};
+
+// task is to update the city to "Lucknow"
+
+student.city = "Lucknow";
+console.log(student);
+
+// task is to add a new key "country" with value "India"
+
+student.country = "India";
+console.log(student);   
