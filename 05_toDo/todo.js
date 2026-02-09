604c39442ded65d969a5a83f5f9f3744928ece95
Garv Gupta <garvg8423@gmail.com>
2026-02-09 17:07:03 +0000

    Refactor formatting and spacing in todo.js for improved readability

--- 05_toDo/todo.js
+++ 05_toDo/todo.js
@@ -1,21 +1,21 @@
 let todo = [];
 let req = prompt("Please enter your request");
 
-while (true){
-    if (req == "quit"){
+while (true) {
+    if (req == "quit") {
         break;
-    } else if (req == "list"){
+    } else if (req == "list") {
         console.log("**********");
-        for (task of todo){
-            console.log (task);
+        for (task of todo) {
+            console.log(task);
         }
-                console.log("**********");
-    } else if (req == "add"){
+        console.log("**********");
+    } else if (req == "add") {
         let task = prompt("Enter a task to add");
         todo.push(task);
         console.log(`${task} added to the list`);
     }
-    else if (req == "delete"){
+    else if (req == "delete") {
         let index = prompt("Enter the index of the task to delete");
         todo.splice(index, 1);
         console.log("Task deleted");
@@ -24,7 +24,7 @@ while (true){
         console.log("Unknown request");
     }
     req = prompt("Please enter your request");
-}   
+}
 
 
 
