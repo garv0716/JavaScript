cf68f9bf13717cef3206ab281935d9e186bf2534
Garv Gupta <garvg8423@gmail.com>
2026-02-11 14:03:11 +0000

    Add error handling example using try-catch and initial console logs in try&Catch.js

--- 10_thisInJs/this.js
+++ 10_thisInJs/this.js
@@ -16,4 +16,8 @@ const student = {
 -> "this" keyword refers to an object that is executing the current piece of code.
 -> jab bhi kisi function ke andar hume uske parameters ko use krrna hota hai to waha par 
    hum "this" keyword ka use krrte hai. 
+-> page par ho sabse higher wali object hoti hai woh "window" object hoti hai. jitna bhi humara code hota hai woh humara windows.object ke andar aata hai.
+
+
+
 */
---
+++ 11_try&Catch/try&Catch.js
@@ -0,0 +1,34 @@
+console.log ("hello");
+console.log ("hello");
+console.log ("hello");
+// let a = 4;
+
+try{  
+ // use this when you find anything suspecioius, like when you are not sure about the code               
+console.log (a);
+}catch(err){
+    console.log("caught an error...a is not define");
+}
+
+
+console.log ("hello");
+console.log ("hello");
+console.log ("hello");
+console.log ("err"); // tells whats the error.
+
+/*
+=>NOTES:
+
+the try statement allow you to define a block of cide to be tested for error whilee it is being exeuted.
+
+the caatch statement alloe oyu to defun a block of codde to be executed if an error occurs in the try block
+
+->Syntax
+
+try{
+console.log(a);
+}catach{
+consloe.log("there is not a "a" variable ")
+}
+
+*/
